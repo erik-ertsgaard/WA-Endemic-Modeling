d29d277441fcd09dbb273713c23bb6ab0fac3c29
erik-ertsgaard <erik.ertsgaard@gmail.com>
2025-01-02 15:20:58 -0800

    establishing files

---
+++ Endemic-Modeling-Script.R
@@ -0,0 +1,22 @@
+##### WASHINGTON ENDEMIC MODELING PROJECT #####
+#### Gjording et al.
+
+# 1.0 LOAD ITEMS -----------------------------------------------------------
+
+# 1.1 Load Packages ----
+
+# 1.2 Load Functions ----
+
+# 1.3 Load Data ----
+
+# 2.0 Data Adjustments -----------------------------------------------------
+
+# 2.1 Filtering Occurence Data ----
+
+# 2.2 Adjusting Predictor Variables ----
+
+# 2.3 Principle Coordinate Analysis (PCA) ----
+
+# 3.0 Species Distribution Model (SDM) -------------------------------------
+
+# 4.0 Figures --------------------------------------------------------------
